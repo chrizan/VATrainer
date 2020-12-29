9d0e477600deafb68a17eee0a0a0b913568868e0
ChriZan <christophzanner@gmx.ch>
2020-12-29 17:08:55 +0100

    Formatting of answers and articles

--- EF_SQLite_Dummy/DatabaseContent/Article.sql
+++ EF_SQLite_Dummy/DatabaseContent/Article.sql
@@ -3,70 +3,41 @@
 VALUES 
 (
 1, 
-'<p><strong>Art. 130 BV &ndash; Mehrwertsteuer</strong></p>
-<p><sup>1</sup>&nbsp;Der Bund kann auf Lieferungen von Gegenst&auml;nden und auf Dienstleistungen einschliesslich Eigenverbrauch sowie auf Einfuhren eine Mehrwertsteuer mit einem Normalsatz von h&ouml;chstens 6,5 Prozent und einem reduzierten Satz von mindestens 2,0 Prozent erheben.</p>
-<p><sup>2</sup>&nbsp;Das Gesetz kann f&uuml;r die Besteuerung der Beherbergungsleistungen einen Satz zwischen dem reduzierten Satz und dem Normalsatz festlegen.</p>
-<p><sup>3</sup>&nbsp;Ist wegen der Entwicklung des Altersaufbaus die Finanzierung der Alters-, Hinterlassenen- und Invalidenversicherung nicht mehr gew&auml;hrleistet, so kann in der Form eines Bundesgesetzes der Normalsatz um h&ouml;chstens 1 Prozentpunkt und der reduzierte Satz um h&ouml;chstens 0,3 Prozentpunkte erh&ouml;ht werden.</p>
-<p><sup>3bis</sup>&nbsp;Zur Finanzierung der Eisenbahninfrastruktur werden die S&auml;tze um 0,1 Prozentpunkte erh&ouml;ht.</p>
-<p><sup>4</sup>&nbsp;5 Prozent des nicht zweckgebundenen Ertrags werden f&uuml;r die Pr&auml;mienverbilligung in der Krankenversicherung zu Gunsten unterer Einkommensschichten verwendet, sofern nicht durch Gesetz eine andere Verwendung zur Entlastung unterer Einkommensschichten festgelegt wird.</p>'
+'<p><strong>Art. 130 BV &ndash; Mehrwertsteuer</strong><br><sup>1</sup>&nbsp;Der Bund kann auf Lieferungen von Gegenst&auml;nden und auf Dienstleistungen einschliesslich Eigenverbrauch sowie auf Einfuhren eine Mehrwertsteuer mit einem Normalsatz von h&ouml;chstens 6,5 Prozent und einem reduzierten Satz von mindestens 2,0 Prozent erheben.<br><sup>2</sup>&nbsp;Das Gesetz kann f&uuml;r die Besteuerung der Beherbergungsleistungen einen Satz zwischen dem reduzierten Satz und dem Normalsatz festlegen.<br><sup>3</sup>&nbsp;Ist wegen der Entwicklung des Altersaufbaus die Finanzierung der Alters-, Hinterlassenen- und Invalidenversicherung nicht mehr gew&auml;hrleistet, so kann in der Form eines Bundesgesetzes der Normalsatz um h&ouml;chstens 1 Prozentpunkt und der reduzierte Satz um h&ouml;chstens 0,3 Prozentpunkte erh&ouml;ht werden.<br><sup>3bis</sup>&nbsp;Zur Finanzierung der Eisenbahninfrastruktur werden die S&auml;tze um 0,1 Prozentpunkte erh&ouml;ht.<br><sup>4</sup>&nbsp;5 Prozent des nicht zweckgebundenen Ertrags werden f&uuml;r die Pr&auml;mienverbilligung in der Krankenversicherung zu Gunsten unterer Einkommensschichten verwendet, sofern nicht durch Gesetz eine andere Verwendung zur Entlastung unterer Einkommensschichten festgelegt wird.</p>'
 ),
 
 (
 2, 
-'<p><strong>Art. 196 BV &ndash; &Uuml;bergangsbestimmungen gem&auml;ss Bundesbeschluss vom 18. Dezember 1998 &uuml;ber eine neue Bundesverfassung </strong><strong><br /></strong></p>
-<p><em>14.</em> &Uuml;bergangsbestimmung zu Art. 130 (Mehrwertsteuer)</p>
-<p><sup>1</sup>&nbsp;Die Befugnis zur Erhebung der Mehrwertsteuer ist bis Ende 2020 befristet.</p>
-<p><sup>2</sup>&nbsp;Zur Sicherung der Finanzierung der Invalidenversicherung hebt der Bundesrat die Mehrwertsteuers&auml;tze vom 1. Januar 2011 bis 31. Dezember 2017 wie folgt an:</p>
-<p><sup>3</sup>&nbsp;Der Ertrag aus der Anhebung nach Absatz 2 wird vollumf&auml;nglich dem Ausgleichsfonds der Invalidenversicherung zugewiesen.</p>
-<p><sup>4</sup>&nbsp;Zur Sicherung der Finanzierung der Eisenbahninfrastruktur hebt der Bundesrat die Steuers&auml;tze nach Artikel 25 des Mehrwertsteuergesetzes vom 12. Juni 2009&nbsp;ab 1. Januar 2018 um 0,1 Prozentpunkt an, im Fall einer Verl&auml;ngerung der Frist gem&auml;ss Absatz 1 bis l&auml;ngstens 31. Dezember 2030.</p>
-<p><sup>5</sup>&nbsp;Der Ertrag aus der Anhebung nach Absatz 4 wird vollumf&auml;nglich dem Fonds nach Artikel 87<em>a</em>&nbsp;zugewiesen.</p>'
+'<p><strong>Art. 196 BV &ndash; &Uuml;bergangsbestimmungen gem&auml;ss Bundesbeschluss vom 18. Dezember 1998 &uuml;ber eine neue Bundesverfassung</strong><br><em>14.</em> &Uuml;bergangsbestimmung zu Art. 130 (Mehrwertsteuer)<br><sup>1</sup>&nbsp;Die Befugnis zur Erhebung der Mehrwertsteuer ist bis Ende 2020 befristet.<br><sup>2</sup>&nbsp;Zur Sicherung der Finanzierung der Invalidenversicherung hebt der Bundesrat die Mehrwertsteuers&auml;tze vom 1. Januar 2011 bis 31. Dezember 2017 wie folgt an:<br><sup>3</sup>&nbsp;Der Ertrag aus der Anhebung nach Absatz 2 wird vollumf&auml;nglich dem Ausgleichsfonds der Invalidenversicherung zugewiesen.<br><sup>4</sup>&nbsp;Zur Sicherung der Finanzierung der Eisenbahninfrastruktur hebt der Bundesrat die Steuers&auml;tze nach Artikel 25 des Mehrwertsteuergesetzes vom 12. Juni 2009&nbsp;ab 1. Januar 2018 um 0,1 Prozentpunkt an, im Fall einer Verl&auml;ngerung der Frist gem&auml;ss Absatz 1 bis l&auml;ngstens 31. Dezember 2030.<br><sup>5</sup>&nbsp;Der Ertrag aus der Anhebung nach Absatz 4 wird vollumf&auml;nglich dem Fonds nach Artikel 87<em>a</em>&nbsp;zugewiesen.</p>'
 ),
 
 (
 3, 
-'<p><strong>Art. 1 MWSTG &ndash; Gegenstand und Grunds&auml;tze</strong></p>
-<p><sup>3</sup>&nbsp;Die Erhebung erfolgt nach den Grunds&auml;tzen:</p>
-<ol>
-<li>der Wettbewerbsneutralit&auml;t;</li>
-<li>der Wirtschaftlichkeit der Entrichtung und der Erhebung;</li>
-<li>der &Uuml;berw&auml;lzbarkeit.</li>
-</ol>'
+'<p><strong>Art. 1 MWSTG &ndash; Gegenstand und Grunds&auml;tze</strong><br><sup>3</sup>&nbsp;Die Erhebung erfolgt nach den Grunds&auml;tzen:<br>a. der Wettbewerbsneutralit&auml;t;<br>b. der Wirtschaftlichkeit der Entrichtung und der Erhebung;<br>c. der &Uuml;berw&auml;lzbarkeit.</p>'
 ),
 
 (
 4, 
-'<p><strong>Art. 3 MWSTG &ndash; Begriff</strong></p>
-<p>Im Sinne dieses Gesetzes bedeuten:</p>
-<ol>
-<li>Leistung: die Einr&auml;umung eines verbrauchsf&auml;higen wirtschaftlichen Wertes an eine Drittperson in Erwartung eines Entgelts, auch wenn sie von Gesetzes wegen oder aufgrund beh&ouml;rdlicher Anordnung erfolgt;</li>
-</ol>'
+'<p><strong>Art. 3 MWSTG &ndash; Begriff</strong><br>Im Sinne dieses Gesetzes bedeuten:<br>c. Leistung: Die Einr&auml;umung eines verbrauchsf&auml;higen wirtschaftlichen Wertes an eine Drittperson in Erwartung eines Entgelts, auch wenn sie von Gesetzes wegen oder aufgrund beh&ouml;rdlicher Anordnung erfolgt;</p>'
 ),
 
 (
 5, 
-'<p><strong>Art. 3 MWSTG &ndash; Begriffe</strong></p>
-<p>Im Sinne dieses Gesetzes bedeuten:</p>
-<ol start="18">
-<li>Inland: das schweizerische Staatsgebiet mit den Zollanschlussgebieten nach Artikel 3 Absatz 2 des Zollgesetzes vom 18. M&auml;rz 20051 (ZG);</li>
-</ol>'
+'<p><strong>Art. 3 MWSTG &ndash; Begriffe</strong><br>Im Sinne dieses Gesetzes bedeuten:<br>a. Inland: Das schweizerische Staatsgebiet mit den Zollanschlussgebieten nach Artikel 3 Absatz 2 des Zollgesetzes vom 18. M&auml;rz 20051 (ZG);</p>'
 ),
 
 (
 6, 
-'<p><strong>Art. 3 ZG &ndash; Zollgebiet, Zollgrenze und Grenzraum</strong></p>
-<p><sup>2</sup> Zollanschlussgebiete sind die ausl&auml;ndischen Gebiete, die auf Grund v&ouml;lkerrechtlicher Vertr&auml;ge oder des V&ouml;lkergewohnheitsrechts zum Zollgebiet geh&ouml;ren.Inland: das schweizerische Staatsgebiet mit den Zollanschlussgebieten nach Artikel 3 Absatz 2 des</p>
-<p>Zollgesetzes vom 18. M&auml;rz 20051 (ZG);</p>'
+'<p><strong>Art. 3 ZG &ndash; Zollgebiet, Zollgrenze und Grenzraum</strong><br><sup>2</sup> Zollanschlussgebiete sind die ausl&auml;ndischen Gebiete, die auf Grund v&ouml;lkerrechtlicher Vertr&auml;ge oder des V&ouml;lkergewohnheitsrechts zum Zollgebiet geh&ouml;ren. Inland: Das schweizerische Staatsgebiet mit den Zollanschlussgebieten nach Artikel 3 Absatz 2 des Zollgesetzes vom 18. M&auml;rz 20051 (ZG);</p>'
 ),
 
 (
 7, 
-'<p><strong>Art. 19 MWSTG &ndash; Mehrheit von Leistungen</strong></p>
-<p><sup>3</sup>&nbsp;Leistungen, die wirtschaftlich eng zusammengeh&ouml;ren und so ineinander greifen, dass sie als unteilbares Ganzes anzusehen sind, gelten als ein einheitlicher wirtschaftlicher Vorgang und sind nach dem Charakter der Gesamtleistung zu behandeln.</p>'
+'<p><strong>Art. 19 MWSTG &ndash; Mehrheit von Leistungen</strong><br><sup>3</sup>&nbsp;Leistungen, die wirtschaftlich eng zusammengeh&ouml;ren und so ineinander greifen, dass sie als unteilbares Ganzes anzusehen sind, gelten als ein einheitlicher wirtschaftlicher Vorgang und sind nach dem Charakter der Gesamtleistung zu behandeln.</p>'
 ),
 
 (
 8, 
-'<p><strong>Art. 19 MWSTG &ndash; Mehrheit von Leistungen</strong></p>
-<p><sup>2</sup>&nbsp;Mehrere voneinander unabh&auml;ngige Leistungen, die zu einer Sachgesamtheit vereinigt sind oder als Leistungskombination angeboten werden, k&ouml;nnen einheitlich nach der &uuml;berwiegenden Leistung behandelt werden, wenn sie zu einem Gesamtentgelt erbracht werden und die &uuml;berwiegende Leistung wertm&auml;ssig mindestens 70 Prozent des Gesamtentgelts ausmacht (Kombination).</p>'
+'<p><strong>Art. 19 MWSTG &ndash; Mehrheit von Leistungen</strong><br><sup>2</sup>&nbsp;Mehrere voneinander unabh&auml;ngige Leistungen, die zu einer Sachgesamtheit vereinigt sind oder als Leistungskombination angeboten werden, k&ouml;nnen einheitlich nach der &uuml;berwiegenden Leistung behandelt werden, wenn sie zu einem Gesamtentgelt erbracht werden und die &uuml;berwiegende Leistung wertm&auml;ssig mindestens 70 Prozent des Gesamtentgelts ausmacht (Kombination).</p>'
 )
 ;
