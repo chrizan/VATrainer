132d55c052386ad3bf10e01bba4a0a4af4465d37
ChriZan <christophzanner@gmx.ch>
2021-03-30 18:01:23 +0200

    FlashCardManager Unit Tests

--- VATrainer.Test/2_ViewModels/FlashCardManagerTest.cs
+++ VATrainer.Test/2_ViewModels/FlashCardManagerTest.cs
@@ -62,6 +62,197 @@ namespace VATrainer.Test.ViewModels
             flashCardManager.NextQuestion.Should().BeNull();
         }
 
+        [Fact]
+        public void Test_Init_No_Question()
+        {
+            // Arrange
+            var repoMock = new Mock<IRepository>();
+            repoMock.Setup(r => r.GetAllQuestionsOfTheme(Theme)).ReturnsAsync(new List<Question>());
+            var flashCardManager = new FlashCardManager(repoMock.Object);
+
+            // Act
+            flashCardManager.Init(Theme);
+
+            // Assert
+            flashCardManager.CardsOnUnconfidentStack.Should().Be(0);
+            flashCardManager.CardsOnSemiConfidentStack.Should().Be(0);
+            flashCardManager.CardsOnConfidentStack.Should().Be(0);
+            flashCardManager.NextQuestion.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_Init_No_NextQuestion()
+        {
+            // Arrange
+            var repoMock = new Mock<IRepository>();
+            var questions = new List<Question>()
+            {
+                new Question() { Stack = (int)CardStack.Left, Order = 1, IsNext = false },
+                new Question() { Stack = (int)CardStack.Left, Order = 2, IsNext = false }
+            };
+            repoMock.Setup(r => r.GetAllQuestionsOfTheme(Theme)).ReturnsAsync(questions);
+            var flashCardManager = new FlashCardManager(repoMock.Object);
+
+            // Act
+            flashCardManager.Init(Theme);
+
+            // Assert
+            flashCardManager.CardsOnUnconfidentStack.Should().Be(2);
+            flashCardManager.CardsOnSemiConfidentStack.Should().Be(0);
+            flashCardManager.CardsOnConfidentStack.Should().Be(0);
+            flashCardManager.NextQuestion.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_No_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_One_Question_Present_Questions_On_Middle_Stack()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_One_Question_Present_No_Questions_On_Middle_Stack()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_Several_Questions_Next_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_Several_Questions_No_Next_Question_Present_Questions_On_Middel_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Left_Stack_Several_Questions_No_Next_Question_Present_No_Questions_On_Middel_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_No_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_One_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_One_Question_Present_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_One_Question_Present_No_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_Several_Questions_Present_Next_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_Several_Questions_Present_No_Next_Question_Present_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Middle_Stack_Several_Questions_Present_No_Next_Question_Present_No_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Unconfident_Right_Stack()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Left_Stack_No_Question()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Left_Stack_One_Question()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Left_Stack_Several_Questions_Next_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Left_Stack_Several_Questions_No_Next_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_No_Question()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_One_Question_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_One_Question_No_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_Several_Questions_Next_Question_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_Several_Questions_No_Next_Question_Present_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Middle_Stack_Several_Questions_No_Next_Question_Present_No_Questions_On_Left_Stack_Present()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Execute_Confident_Right_Stack()
+        {
+
+        }
+
         private Mock<IRepository> GetRepositoryMock(
             int questionsOnLeftStack,
             int questionsOnMiddleStack,
